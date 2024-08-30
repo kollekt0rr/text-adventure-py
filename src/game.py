aa4e12a21e417b31f26ef24550b7c86fd27a34c5
Arturo Torres Jimenez <art0rresjmz@gmail.com>
2024-08-29 20:35:10 -0700

    some more small project commits

---
+++ src/game.py
@@ -0,0 +1,7 @@
+#imports
+import os, random #some additional when new features are added
+
+#global variables
+game_loop = True
+initial_menu = True
+
---
+++ src/main.py
@@ -0,0 +1,6 @@
+#imports
+import game #possibly some more modules when DLC is created
+
+#main loop
+if __name__ == "__main__":
+    game()
